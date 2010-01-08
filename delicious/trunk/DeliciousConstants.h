5294710372c61032597b3d728b9ab7425fc976a4
nparry <nparry@16fc46a2-29eb-11de-9f0d-3bffa153b7de>
2010-01-08 23:42:41 +0000

    Move Delicious and Rot13 plugins from Github

---
+++ delicious/trunk/DeliciousConstants.h
@@ -0,0 +1,5 @@
+
+#define kDeliciousLastUpdateURL @"https://api.del.icio.us/v1/posts/update"
+#define kDeliciousAllBookmarksURL @"https://api.del.icio.us/v1/posts/all"
+#define kDeliciousPluginUserAgent @"GoogleQuickSearchBoxDeliciousPlugin/0.4"
+#define kObjectAttributeDeliciousTags @"ObjectAttributeDeliciousTags"
