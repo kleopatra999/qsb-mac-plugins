215cfbe281609a751a2bbdf3f14f09506c103f9d
dmaclach@gmail.com <dmaclach@gmail.com@16fc46a2-29eb-11de-9f0d-3bffa153b7de>
2010-04-14 20:57:56 +0000

    Add an upload script to AllPlugins to allow me to quickly update plugins.

--- trunk/openmeta/openmetaAction.m
+++ trunk/openmeta/openmetaAction.m
@@ -7,29 +7,118 @@
 //
 
 #import <Vermilion/Vermilion.h>
+#import "OpenMeta.h"
+#import "OpenMetaPrefs.h"
 
-@interface openmetaAction : HGSAction
+@interface OpenMetaAction : HGSAction
+- (NSArray *)tagsFromResult:(HGSResult *)result;
 @end
 
-@implementation  openmetaAction
+@interface OpenMetaActionAddTag : HGSAction
+@end
 
-// Perform an action given a dictionary of info.
-- (BOOL)performWithInfo:(NSDictionary *)info {
-  HGSResultArray *directObjects
-    = [info objectForKey:kHGSActionDirectObjectsKey];
-  BOOL success = NO;
-  if (directObjects) {
-    NSString *name = [directObjects displayName];
-    NSString *localizedOK = HGSLocalizedString(@"OK", nil);
-    NSString *localizedFormat = HGSLocalizedString(@"Action performed on %@",
-                                                   nil);
-    [NSAlert alertWithMessageText:NSStringFromClass([self class])
-                    defaultButton:localizedOK
-                   alternateButton:nil
-                       otherButton:nil
-         informativeTextWithFormat:localizedFormat, name];
-    success = YES;
+@interface OpenMetaActionRemoveTag : HGSAction
+@end
+
+@interface OpenMetaActionTagArgument : HGSActionArgument
+@end
+
+static NSArray *TagsFromInfo(NSDictionary *info) {
+  HGSResultArray *tags
+    = [info objectForKey:@"com.google.qsb.openmeta.action.argument.tag"];
+  NSMutableArray *textTags = [NSMutableArray arrayWithCapacity:[tags count]];
+  for (HGSResult *tag in tags) {
+    NSDictionary *value 
+      = [tag valueForKey:kHGSObjectAttributePasteboardValueKey];
+    NSString *textValue = [value objectForKey:NSStringPboardType];
+    if (textValue) {
+      [textTags addObject:textValue];
+    } else {
+      HGSLogDebug(@"Couldn't get text from %@", tag);
+    }
   }
-  return success;
+  return textTags;
 }
+
+@implementation  OpenMetaActionAddTag
+
+- (BOOL)performWithInfo:(NSDictionary *)info {
+  BOOL wasGood = YES;
+  NSArray *tags = TagsFromInfo(info);
+  HGSResultArray *directObjects 
+    = [info objectForKey:kHGSActionDirectObjectsKey];
+  for (HGSResult *file in directObjects) {
+    NSString *filePath = [file filePath];
+    NSError *error;
+    NSArray *oldFileTags = [OpenMeta getUserTags:filePath error:&error];
+    if (error) {
+      [NSApp presentError:error];
+      wasGood = NO;
+      break;
+    }
+    error = [OpenMeta addUserTags:tags path:filePath];
+    if (error) {
+      [NSApp presentError:error];
+      wasGood = NO;
+      break;
+    }
+    [OpenMetaPrefs updatePrefsRecentTags:oldFileTags newTags:tags];
+  }
+  return wasGood;
+}
+
 @end
+
+@implementation  OpenMetaActionRemoveTag
+
+- (BOOL)performWithInfo:(NSDictionary *)info {
+  BOOL wasGood = YES;
+  NSArray *tags = TagsFromInfo(info);
+  HGSResultArray *directObjects 
+    = [info objectForKey:kHGSActionDirectObjectsKey];
+  for (HGSResult *file in directObjects) {
+    NSString *filePath = [file filePath];
+    NSError *error = [OpenMeta clearUserTags:tags path:filePath];
+    if (error) {
+      [NSApp presentError:error];
+      wasGood = NO;
+      break;
+    }
+  }
+  return wasGood;
+}
+
+@end
+
+@implementation OpenMetaActionTagArgument
+
+- (NSArray *)resultsForQuery:(HGSQuery *)query source:(HGSSearchSource *)source {
+  NSArray *recentTags = [OpenMetaPrefs recentTags];
+  NSMutableArray *scoredTags 
+    = [NSMutableArray arrayWithCapacity:[recentTags count]];
+  NSWorkspace *ws = [NSWorkspace sharedWorkspace];
+  NSString *fileType = NSFileTypeForHFSTypeCode(kClippingTextType);
+  NSImage *image = [ws iconForFileType:fileType];
+  HGSTokenizedString *tokenizedQueryString = [query tokenizedQueryString];
+  for (NSString *recentTag in recentTags) {
+    NSDictionary *attributes
+      = [NSDictionary dictionaryWithObjectsAndKeys:
+         image, kHGSObjectAttributeIconKey,
+         nil];
+    HGSScoredResult *hgsObject
+      = [HGSScoredResult resultWithURI:@"openmetatag:text"
+                                  name:recentTag
+                                  type:kHGSTypeTextUserInput
+                                source:source
+                            attributes:attributes
+                                 score:HGSCalibratedScore(kHGSCalibratedPerfectScore)
+                                 flags:eHGSSpecialUIRankFlag
+                           matchedTerm:tokenizedQueryString
+                        matchedIndexes:nil];
+    [scoredTags addObject:hgsObject];
+  }
+  return scoredTags;
+}
+
+@end
+
